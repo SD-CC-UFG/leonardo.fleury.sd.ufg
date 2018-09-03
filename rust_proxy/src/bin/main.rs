54156be3ffd28c6e92a605a0665fe17b251a55c1
Leonardo Fleury <fleuryleomoraes@gmail.com>
2018-09-03 16:48:31 -0300

    Echo proxy

--- rust_proxy/src/bin/main.rs
+++ rust_proxy/src/bin/main.rs
@@ -1,51 +1,21 @@
 extern crate ruschat_server;
-extern crate url;
-extern crate http_muncher;
 
 use std::str;
-use ruschat_server::ThreadPool;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::net::Shutdown;
-use http_muncher::{Parser, ParserHandler};
 
-struct RequestHandler{}
-
-impl ParserHandler for RequestHandler{
-    fn on_url(&mut self, parser: &mut Parser, url: &[u8]) -> bool {
-        println!("url: {:?}", str::from_utf8(url).unwrap());
-        true
-    }
-
-    fn on_body(&mut self, parser: &mut Parser, body: &[u8]) -> bool {
-        println!("body: {:?}", str::from_utf8(body).unwrap());
-        true
-    }
-
-    fn on_header_field(&mut self, parser: &mut Parser, header: &[u8]) -> bool {
-       println!("header: {}: ", str::from_utf8(header).unwrap());
-       true
-   }
-
-   fn on_header_value(&mut self, parser: &mut Parser, value: &[u8]) -> bool {
-       println!("\t {:?}", str::from_utf8(value).unwrap());
-       true
-   }
-}
+use ruschat_server::ThreadPool;
 
 fn handle_client(mut stream: TcpStream) {
     loop {
-        let mut parser = Parser::request();
         let mut buf = [0; 1024];
 
         stream.read(&mut buf).unwrap();
+        
+        let req = str::from_utf8(& buf);
 
-        let message = String::from_utf8_lossy(&buf[..]);
-
-        parser.parse(&mut RequestHandler {}, message.as_bytes());
-
-        println!("{:?}", parser);
+        println!("{:#?}", req);
     }
 }
 
@@ -53,13 +23,17 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
     let pool = ThreadPool::new(32);
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
+    println!("Servidor iniciado em {}", listener.local_addr().unwrap());
 
-        //TODO: Escolher em qual thread executar
-        pool.execute(|| {
-            handle_client(stream);
-        });
+    for stream in listener.incoming() {
+        match stream {
+            Ok(s) => {
+                pool.execute(|| {
+                    handle_client(s);
+                });
+            },
+            Err(e) => panic!("encountered IO error: {}", e),
+        }
     }
 
     println!("Desligando!");
