ca9884b26174baac19e9716d2bc2aef6fc256e9c
Leonardo Moraes Fleury <fleuryleomoraes@gmail.com>
2018-09-24 15:39:25 -0300

    Requrimentos do server

--- todo-app/server/app/__init__.py
+++ todo-app/server/app/__init__.py
@@ -1,5 +1,6 @@
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
@@ -9,6 +10,9 @@ def create_app():
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    CORS(app)
+    
     app.register_blueprint(todo_page)
     db.init_app(app)
 
