6335ecad01d3cae024970d9f943f0bd6368ac78e
Leonardo Fleury <fleuryleomoraes@gmail.com>
2018-11-26 14:13:47 -0200

    URL working as expected

--- notes/main.py
+++ notes/main.py
@@ -1,4 +1,5 @@
 import datetime
+from bson.objectid import ObjectId
 
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify
@@ -8,13 +9,14 @@ from notes.database import get_db
 
 bp = Blueprint('notes', __name__)
 
+
 @bp.route('/', methods=['POST'])
 def create_note():
     if request.is_json:
-        note = request.json
+        data = request.json
         mongo = get_db()
-        note = {'title': note['title'],
-                'text': note['text'],
+        note = {'title': data['title'],
+                'text': data['text'],
                 'created': datetime.datetime.utcnow(),
                 'last_updated': datetime.datetime.utcnow()}
         note_id = mongo.db.notes.insert_one(note).inserted_id
@@ -22,18 +24,24 @@ def create_note():
 
     return "Not JSON"
 
-@bp.route('/<uuid:note_id>', methods=['PUT'])
+
+@bp.route('/<note_id>', methods=['PUT'])
 def update_note(note_id):
-    with request.get_json as data:
+    if request.is_json:
+        data = request.json
         mongo = get_db()
-        note = {'title': note['title'],
-                'text': note['text'],
-                'last_updated': datetime.datetime.utcnow()}
-        mod_count = mongo.db.notes.update_one({'_id': note_id}, note).modified_count
+
+        note = {'$currentDate': {'last_updated': True},
+                '$set': {'title': data['title'],
+                         'text': data['text']}}
+        mod_count = mongo.db.notes.update_one(
+            {'_id': ObjectId(note_id)}, note).modified_count
+
         return "{} notes updated".format(mod_count)
 
-@bp.route('/<uuid:note_id>', methods=['DELETE'])
+
+@bp.route('/<note_id>', methods=['DELETE'])
 def delete_note(note_id):
     mongo = get_db()
-    del_count = mongo.db.notes.delete_one({'_id': note_id}).deleted_count
-    return "Note deleted"
+    del_count = mongo.db.notes.delete_one({'_id': ObjectId(note_id)}).deleted_count
+    return "{} note deleted".format(del_count)
