417d73e3337ac550f8cf903776a7dff0c6485349
Leonardo Moraes Fleury <fleuryleomoraes@gmail.com>
2018-11-26 06:47:16 -0200

    first drafts of routes functions

--- notes/main.py
+++ notes/main.py
@@ -10,12 +10,30 @@ bp = Blueprint('notes', __name__)
 
 @bp.route('/', methods=['POST'])
 def create_note():
-    pass
+    if request.is_json:
+        note = request.json
+        mongo = get_db()
+        note = {'title': note['title'],
+                'text': note['text'],
+                'created': datetime.datetime.utcnow(),
+                'last_updated': datetime.datetime.utcnow()}
+        note_id = mongo.db.notes.insert_one(note).inserted_id
+        return "{}".format(note_id)
 
-@bp.route('/<int:note_id>', methods=['PUT'])
+    return "Not JSON"
+
+@bp.route('/<uuid:note_id>', methods=['PUT'])
 def update_note(note_id):
-    pass
+    with request.get_json as data:
+        mongo = get_db()
+        note = {'title': note['title'],
+                'text': note['text'],
+                'last_updated': datetime.datetime.utcnow()}
+        mod_count = mongo.db.notes.update_one({'_id': note_id}, note).modified_count
+        return "{} notes updated".format(mod_count)
 
-@bp.route('/<int:note_id>', methods=['DELETE'])
+@bp.route('/<uuid:note_id>', methods=['DELETE'])
 def delete_note(note_id):
-    pass
+    mongo = get_db()
+    del_count = mongo.db.notes.delete_one({'_id': note_id}).deleted_count
+    return "Note deleted"
