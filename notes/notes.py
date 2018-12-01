afec4e07e6d39d5fff96cd6ffc2c9090f206352c
Leonardo Moraes Fleury <fleuryleomoraes@gmail.com>
2018-12-01 18:39:53 -0200

    Add view functions as return as JSON

--- notes/notes.py
+++ notes/notes.py
@@ -2,6 +2,7 @@ import datetime
 
 from nameko.rpc import rpc
 from bson.objectid import ObjectId
+from bson.json_util import dumps
 from notes.database import get_db
 
 
@@ -16,7 +17,7 @@ class Note(object):
                 'created': datetime.datetime.utcnow(),
                 'last_updated': datetime.datetime.utcnow()}
         note_id = mongo.db.notes.insert_one(note).inserted_id
-        return "{}".format(note_id)
+        return dumps(note_id)
 
     @rpc
     def update_note(self, note_id, title, text):
@@ -28,11 +29,27 @@ class Note(object):
         mod_count = mongo.db.notes.update_one(
             {'_id': ObjectId(note_id)}, note).modified_count
 
-        return "{} notes updated".format(mod_count)
+        return dumps(mod_count)
 
     @rpc
     def delete_note(self, note_id):
         mongo = get_db()
         del_count = mongo.db.notes.delete_one(
             {'_id': ObjectId(note_id)}).deleted_count
-        return "{} note deleted".format(del_count)
+        return dumps(del_count)
+
+    @rpc
+    def view_note(self, note_id):
+        mongo = get_db()
+
+        note = mongo.db.notes.find_one({"_id": ObjectId(note_id)})
+
+        return dumps(note)
+
+    @rpc
+    def view_all_notes(self):
+        mongo = get_db()
+
+        notes = mongo.db.notes.find()
+
+        return dumps(notes)
--- notes/tests/conftest.py
+++
@@ -1,6 +0,0 @@
-import os
-import tempfile
-
-import pytest
-from notes import create_app
-from notes.db import get_db, init_db
