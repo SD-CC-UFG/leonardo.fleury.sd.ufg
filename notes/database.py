b45ba493e6cc8e689d105b1f177366f3f0d8da33
Leonardo Moraes Fleury <fleuryleomoraes@gmail.com>
2018-12-02 19:48:08 -0200

    Using one document per user

--- notes/database.py
+++ notes/database.py
@@ -1,5 +1,9 @@
 from pymongo import MongoClient
 
 
-def get_db():
-    return MongoClient("mongodb+srv://notes_admin:GhUTriaIlUzOVmIC@notesdb-ytix1.gcp.mongodb.net/notes?retryWrites=true").db.notes
+def get_db(user=None):
+    if user:
+        return MongoClient("mongodb+srv://notes_admin:GhUTriaIlUzOVmIC@notesdb-ytix1.gcp.mongodb.net/notes?retryWrites=true").notes[user]
+    else:
+        raise Exception("User was not set.")
+
--- notes/notes.py
+++ notes/notes.py
@@ -10,46 +10,53 @@ class Note(object):
     name = "notes"
 
     @rpc
-    def create_note(self, title, text):
-        mongo = get_db()
+    def create_note(self, user, title, text):
+        mongo = get_db(user)
+
         note = {'title': title,
                 'text': text,
+                'user_id': user,
                 'created': datetime.datetime.utcnow(),
                 'last_updated': datetime.datetime.utcnow()}
+
         note_id = mongo.insert_one(note).inserted_id
+
         return dumps(note_id)
 
     @rpc
-    def update_note(self, note_id, title, text):
-        mongo = get_db()
+    def update_note(self, user, note_id, title, text):
+        mongo = get_db(user)
 
         note = {'$currentDate': {'last_updated': True},
                 '$set': {'title': title,
                          'text': text}}
+
         mod_count = mongo.update_one(
             {'_id': ObjectId(note_id)}, note).modified_count
 
         return dumps(mod_count)
 
     @rpc
-    def delete_note(self, note_id):
-        mongo = get_db()
+    def delete_note(self, user, note_id):
+        mongo = get_db(user)
+
         del_count = mongo.delete_one(
             {'_id': ObjectId(note_id)}).deleted_count
+
         return dumps(del_count)
 
     @rpc
-    def view_note(self, note_id):
-        mongo = get_db()
+    def view_note(self, user, note_id):
+        mongo = get_db(user)
 
         note = mongo.find_one({"_id": ObjectId(note_id)})
 
         return dumps(note)
 
     @rpc
-    def view_all_notes(self):
-        mongo = get_db()
+    def view_all_notes(self, user):
+        mongo = get_db(user)
 
-        notes = mongo.find()
+        notes = mongo.find({'user': user})
 
         return dumps(notes)
