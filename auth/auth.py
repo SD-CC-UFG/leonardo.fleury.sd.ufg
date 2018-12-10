2d47ce043098b99fbb87344de18bcfc4762f6a0f
Leonardo Moraes Fleury <fleuryleomoraes@gmail.com>
2018-12-09 23:51:02 -0200

    Removing token generation

--- auth/auth.py
+++ auth/auth.py
@@ -1,6 +1,7 @@
 import jwt
 import logging
 import yaml
+import json
 
 from datetime import datetime, timedelta
 
@@ -24,37 +25,19 @@ class Auth(object):
 
         if user:
             if (hashpw(password.encode('utf-8'), user['password']) == user['password']):
-                token = self.__encode_auth_token(username)
                 log.info("User was authenticate.")
-                return dumps(token)
+                return json.dumps({
+                    "code": 0
+                })
             else:
-                log.info("User was not authenticate.")
-                return 1
+                log.info("Incorrect password.")
+                return json.dumps({
+                    "code": 1,
+                    "error": "Incorrect password."
+                })
         else:
-            log.info("User was not authenticate.")
-            return 1
-
-    def __encode_auth_token(self, username):
-        try:
-            payload = {
-                'iss': 'notes auth server',
-                'exp': datetime.utcnow() + timedelta(days=0, seconds=5),
-                'iat': datetime.utcnow(),
-                'sub': username
-            }
-            return jwt.encode(
-                payload,
-                config['SECRET_KEY'],
-                algorithm='HS256'
-            )
-        except Exception as e:
-            return e
-
-    def __decode_auth_token(self, auth_token):
-        try:
-            payload = jwt.decode(auth_token, config['SECRET_KEY'])
-            return payload['sub']
-        except jwt.ExpiredSignatureError:
-            return 'Signature expired. Please log in again.'
-        except jwt.InvalidTokenError:
-            return 'Invalid token. Please log in again.'
+            log.info("Could not find user.")
+            return json.dumps({
+                    "code": 1,
+                    "error": "Could not find user."
+                })
